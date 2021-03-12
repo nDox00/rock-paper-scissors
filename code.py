e066bf0f28b9c37ca73306eb92e09a4b24121cbd
nDox00 <80198134+nDox00@users.noreply.github.com>
2021-03-12 15:09:18 +0200

    Update code.py

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
-def play_now():
+from random import randint
+def rock_paper_scissors():
     print("Welcome to Rock, Paper Scissors")
     
     play = input("Do you wanna play? Y/N").title()    
@@ -6,12 +7,10 @@ def play_now():
         start = True
     elif play == 'N':
         start = False
-    return start
-  
-from random import randint
-def rock_paper_scissors():
-    while True:
-        print("1: Rock,\n 2: Paper, \n 3: Scissors")
+        print("Thank you for playing")
+
+    while start == True:
+        print(" 1: Rock,\n 2: Paper, \n 3: Scissors")
         player_choice = int(input("Chose: "))
         if player_choice <= 3:
             print("That's Okay")
@@ -19,19 +18,21 @@ def rock_paper_scissors():
             if player_choice >=4:
                 print("Invalid Choice, try again")
                 continue
-        
-            
-        
-
         pc_choice = randint(1,3)
+        print(f'Your choice is {player_choice}')
+        print(f'PC choice is {pc_choice}')
+   
     
         if player_choice == pc_choice:
             print("It's a tie!")
     
-        elif player_choice == 3 and pc_choice == 2 or player_choice ==1:
+        elif player_choice == 3 and pc_choice == 2 or player_choice ==2 and pc_choice == 1 or player_choice ==1 and pc_choice == 3 :
             print("Player Wins!! Woop woop")
         else:
             print('Robocop wins :(')
-
-            print(f'Your choice is {player_choice}')
-            print(f'PC choice is {pc_choice}')
+        
+        break
+        
+    play_again = input("Do you want to play again? Y/N")
+    play_again = play
+    
